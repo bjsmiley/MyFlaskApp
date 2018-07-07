e516a6392d732c4e430f5bce6b8f5b6991086991
Bryan Smiley <bjsmiley@ucdavis.edu>
2018-07-06 19:16:44 -0700

    server can run w/ routes beginning to be implemented

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    Articles = [
+        {
+            'id':1,
+            'title':'Article 1',
+            'body':'asdfrefref',
+            'author': 'Bryan Smiley',
+            'create_date':'04-25-2017'
+        },
+        {
+            'id':2,
+            'title':'Article 2',
+            'body':'asdfresdvefvrvrvgfref',
+            'author': 'Bryan Smiley',
+            'create_date':'04-25-2017'
+        },
+        {
+            'id':3,
+            'title':'Article 3',
+            'body':'asdfrefref',
+            'author': 'Dj khalic Smiley',
+            'create_date':'04-25-2017'
+        }
+    ]
+
+    return Articles
